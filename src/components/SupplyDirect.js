994374b480053879cfd24b36461bccf52762959f
eddy0 <awindy369@gmail.com>
2018-05-27 10:35:14 -0700

    update
    update

--- src/components/SupplyDirect.js
+++ src/components/SupplyDirect.js
@@ -2,13 +2,23 @@ import React from 'react'
 
 class SupplyDirect extends React.Component {
 
+    state={
+        list: {
+            title: '',
+            price:'',
+            newItem: false,
+        }
+    }
+
     render() {
         return (
             <section className='supply'>
                 <header className='supply-title'>
                     <span>品牌制造商直供</span>
-
                 </header>
+                <div className="supply-list">
+
+                </div>
             </section>
 
         )
